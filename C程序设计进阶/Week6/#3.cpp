8accf4bfb3ef34c5451f1ab1bde34c46296e46fc
Sookie <suqisookie@gmail.com>
2016-06-23 08:05:00 +0800

    no message

--- C程序设计进阶/Week6/#3.cpp
+++ C程序设计进阶/Week6/#3.cpp
@@ -34,3 +34,16 @@
 8 4
 9 3
 **************************************************/
+#include <iostream>
+using namespace std;
+int main(){
+	int row, col;
+	cin >> row >>col;
+	int datas[row][col];
+	for (int i = 0 ; i < row; i ++) {
+		for (int j = 0; j < col; j++) {
+			cin >> datas[row][col];
+		}
+	}
+	return 0;
+}
